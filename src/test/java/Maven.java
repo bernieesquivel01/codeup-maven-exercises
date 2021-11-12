f05cf037529ae1fc05fa9c2b1f18dfc49860d0c9
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-11-12 13:18:54 -0600

    feat: maven exercise complete

---
+++ src/test/java/Maven.java
@@ -0,0 +1,29 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Maven {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter Something: ");
+        String userChoice = userInput.nextLine();
+
+        System.out.printf("You Entered: \"%s\"", userChoice);
+        System.out.println();
+
+        String userEnteredNum;
+        if(StringUtils.isNumeric(userChoice)){
+            userEnteredNum = "is a number";
+        } else {
+            userEnteredNum = "is not a number";
+        }
+
+        System.out.printf("\"%s\" %s", userChoice, userEnteredNum);
+        System.out.println();
+        System.out.printf("Flipped Case: %s", StringUtils.swapCase(userChoice));
+        System.out.println();
+        System.out.printf("Reversed: %s", StringUtils.reverse(userChoice));
+    }
+}
